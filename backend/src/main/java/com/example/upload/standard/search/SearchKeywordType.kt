71238657ecf7271ac2e52260afa70bdca9faa67e
asungkim <asungkim007@naver.com>
2025-03-25 17:14:46 +0900

    SearKeywordType

---
+++ backend/src/main/java/com/example/upload/standard/search/SearchKeywordType.kt
@@ -0,0 +1,9 @@
+package com.example.upload.standard.search
+
+enum class SearchKeywordType(val value: String) {
+    title("title"),
+    content("content"),
+    author("author"),
+    all("all");
+
+}
