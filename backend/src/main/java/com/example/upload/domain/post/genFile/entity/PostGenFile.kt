df2cace128bdf40afc1af7255f51c888abfcbc2b
asungkim <asungkim007@naver.com>
2025-03-25 10:16:26 +0900

    PostGenFile 전환

---
+++ backend/src/main/java/com/example/upload/domain/post/genFile/entity/PostGenFile.kt
@@ -0,0 +1,57 @@
+package com.example.upload.domain.post.genFile.entity;
+
+import com.example.upload.domain.base.genFile.genFile.entity.GenFile
+import com.example.upload.domain.post.post.entity.Post;
+import jakarta.persistence.*;
+import org.springframework.expression.spel.ast.TypeCode;
+
+@Entity
+class PostGenFile : GenFile {
+    @ManyToOne(fetch = FetchType.LAZY)
+    lateinit var post: Post
+
+    @Enumerated(EnumType.STRING)
+    lateinit var typeCode:
+    TypeCode
+
+    enum class TypeCode {
+        attachment,
+        thumbnail
+    }
+
+    constructor(
+            post: Post,
+            typeCode: TypeCode,
+            fileNo: Int,
+            originalFileName: String,
+            metadataStr: String,
+            yyyyMmDd: String,
+            fileExtTypeCode: String,
+            fileExtType2Code: String,
+            fileExt: String,
+            fileName: String,
+            fileSize: Long
+    ) : super(
+            fileNo,
+            originalFileName,
+            metadataStr,
+            yyyyMmDd,
+            fileExtTypeCode,
+            fileExtType2Code,
+            fileExt,
+            fileName,
+            fileSize
+    ) {
+        this.post = post
+        this.typeCode = typeCode
+    }
+
+    override fun getOwnerModelId(): Long {
+        return post.id
+    }
+
+    override fun getTypeCodeAsStr(): String {
+        return typeCode.name
+    }
+
+}
