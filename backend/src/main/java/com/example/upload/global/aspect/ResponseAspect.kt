ac296fe1352a9cdbafdaa11e5acf599935bd92a3
asungkim <asungkim007@naver.com>
2025-03-26 12:16:16 +0900

    ResponseAspect

--- backend/src/main/java/com/example/upload/global/aspect/ResponseAspect.kt
+++ backend/src/main/java/com/example/upload/global/aspect/ResponseAspect.kt
@@ -1,21 +1,20 @@
 package com.example.upload.global.aspect;
 
-import com.example.upload.global.dto.RsData;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
+import com.example.upload.global.dto.RsData
+import jakarta.servlet.http.HttpServletResponse
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.springframework.stereotype.Component
 
 @Aspect
 @Component
-@RequiredArgsConstructor
-public class ResponseAspect {
+class ResponseAspect(
+    private val response: HttpServletResponse
+) {
 
-    private final HttpServletResponse response;
-
-    @Around("""
+    @Around(
+        """
             (
                 within
                 (
@@ -34,16 +33,17 @@ public class ResponseAspect {
             )
             ||
             @annotation(org.springframework.web.bind.annotation.ResponseBody)
-            """)
-    public Object responseAspect(ProceedingJoinPoint joinPoint) throws Throwable {
-        Object rst = joinPoint.proceed(); // 실제 수행 메서드
+            """
+    )
+    fun responseAspect(joinPoint: ProceedingJoinPoint): Any {
+        val rst = joinPoint.proceed() // 실제 수행 메서드
 
-        if(rst instanceof RsData rsData) {
-            int statusCode = rsData.getStatusCode();
-            response.setStatus(statusCode);
+        if (rst is RsData<*>) {
+            val statusCode = rst.statusCode
+            response.status = statusCode
         }
 
-        return rst; // json으로 변환되어 응답
+        return rst
     }
 
 }
