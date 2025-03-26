4ee470650fcd0b9afeb067dcd181bb66c11cde75
asungkim <asungkim007@naver.com>
2025-03-26 12:40:54 +0900

    HomeController

---
+++ backend/src/main/java/com/example/upload/domain/home/controller/HomeController.kt
@@ -0,0 +1,36 @@
+package com.example.upload.domain.home.controller;
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.servlet.http.HttpSession
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import java.util.*
+
+@Tag(name = "HomeController", description = "API 서버 홈")
+@Controller
+class HomeController {
+
+    @Operation(summary = "API 서버 시작페이지", description = "API 서버 시작페이지입니다. api 호출은 인증을 해주세요")
+    @GetMapping(value = ["/"], produces = ["text/plain;charset=UTF-8"])
+    @ResponseBody
+    fun home(): String {
+        return "API 서버에 오신 걸 환영합니다."
+    }
+
+    @GetMapping("/session")
+    @ResponseBody
+    fun session(session: HttpSession): Map<String, Any> {
+        val sessionMap = mutableMapOf<String, Any>()
+        val names: Enumeration<String> = session.attributeNames
+
+        while (names.hasMoreElements()) {
+            val name = names.nextElement()
+            sessionMap[name] = session.getAttribute(name)
+        }
+
+        return sessionMap
+    }
+
+}
